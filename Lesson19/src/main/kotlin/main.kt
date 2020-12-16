b11b6804146f7605c6b3c8b4dc4e1822df8e3158
Luís Falcão <lfalcao@cc.isel.pt>
2020-12-16 11:47:43 +0000

    Lesson 19: Copy from Lesson 18

---
+++ Lesson19/src/main/kotlin/main.kt
@@ -0,0 +1,52 @@
+import pt.isel.canvas.*
+
+/**
+ * Grid width in blocks
+ */
+const val GRID_WIDTH = 6
+
+/**
+ * Grid height in blocks
+ */
+const val GRID_HEIGHT = 10
+
+/**
+ * Valid horizontal block positions.
+ */
+val GRID_X = 0 until GRID_WIDTH
+
+/**
+ * Valid vertical block positions.
+ */
+val GRID_Y = 0 until GRID_HEIGHT
+
+/**
+ * Program entry point for a version of Tetris game.
+ *
+ * Movement of a block in a grid (10x20) using the cursor keys.
+ * The block is fixed when it touches the base or on top of another block.
+ * A new block appears with a random color.
+ */
+fun main() {  // ()->Unit
+    onStart {
+        val arena = Canvas(GRID_WIDTH*BLOCK_SIDE, GRID_HEIGHT*BLOCK_SIDE, BLACK)
+        var game = Tetris( newPiece(), emptyList() )
+        arena.drawTetris(game)
+        arena.onKeyPressed { ke ->
+            val g :Tetris? = when(ke.code) {
+                LEFT_CODE -> game.move(-1,0)
+                RIGHT_CODE -> game.move(+1,0)
+                DOWN_CODE -> game.move(0,+1)
+                UP_CODE -> game.rotate()
+                else -> null
+            }
+            if (g!=null) {
+                game = g
+                arena.drawTetris(game)
+            }
+        }
+    }
+    onFinish { }
+}
+
+
---
+++ Lesson19/src/main/kotlin/piece.kt
@@ -0,0 +1,45 @@
+import pt.isel.canvas.*
+
+data class Piece(val type:Tetromino, val blks:List<Block>)
+
+enum class Tetromino(val color:Int) { J(BLUE), O(YELLOW), I(CYAN) } // ,L,S,Z,T
+
+val tetrominos = listOf('J','O','I') //,'L','S','Z','T')
+
+fun  createPiece(t:Tetromino,x1:Int,y1:Int,x2:Int,y2:Int,x3:Int,y3:Int) :Piece {
+    val x = GRID_WIDTH / 2
+    val y = 1
+    val color = t.color
+    return Piece( t, listOf (
+       Block(x,y,color), Block(x+x1,y+y1,color), Block(x+x2,y+y2,color),Block(x+x3,y+y3,color)
+    ))
+}
+
+fun newPiece() = when(Tetromino.values().random()) {
+        Tetromino.J-> createPiece(Tetromino.J,0,-1,0,1,-1,1)
+        Tetromino.I -> createPiece(Tetromino.I,0,-1,0,1,0,2)
+        else -> createPiece(Tetromino.O,0,-1,1,-1,1,0)
+    }
+
+fun Canvas.drawPiece(p:Piece)  {
+    p.blks.forEach { drawBlock(it) }
+}
+
+fun Piece.move(dx:Int, dy:Int, f:List<Block>) :Piece? {
+    val bs:List<Block> = blks.mapNotNull { b -> b.move(dx, dy, f) }
+    return if (bs.size==4) Piece(type,bs) else null
+}
+
+fun Piece.toFix(f:List<Block>) = blks.any { it.toFix(f) }
+
+fun Piece.rotate(f:List<Block>):Piece? {
+    if (type==Tetromino.O) return null
+    val bs = blks.mapNotNull{ b -> b.rotate(blks[0],f) }
+    return if (bs.size==4) Piece(type,bs) else null
+}
+
+fun Block.rotate(center:Block, f:List<Block>) :Block? {
+    if (this==center) return this
+    val b = Block(center.x -y +center.y, center.y +x - center.x, color)
+    return if (b.valid(f)) b else null
+}
